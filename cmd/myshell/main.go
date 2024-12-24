619267a21eb230474fea699e7cb598f7463fb698
vietbui99 <buiquocviet99@gmail.com>
2024-12-23 22:48:11 -0500

    test

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -41,13 +41,13 @@ func main() {
     // Wait for user input
     input, err := bufio.NewReader(os.Stdin).ReadString('\n')
     trimmpedInput := strings.TrimSpace(input)
-    fmt.Printf("%s: command not found\n", trimmpedInput)
+    // fmt.Printf("%s: command not found\n", trimmpedInput)
     
     if err != nil {
       fmt.Printf("%s: invalid input\n", input)
     }
 
-    //handleCommands(trimmpedInput)
+    handleCommands(trimmpedInput)
   }
 }
 
