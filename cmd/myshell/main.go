4f624765d05d21ded35a76b64d70e8d826aaff79
vietbui99 <buiquocviet99@gmail.com>
2024-12-23 22:46:01 -0500

    test

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,15 +4,55 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
+  "strconv"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
-func main() {
-	// Uncomment this block to pass the first stage
-	// fmt.Fprint(os.Stdout, "$ ")
+var commands map[string]func(string)
 
-	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+func init() {
+  commands = make(map[string]func(string)) 
+  commands["exit"] = exitCommand
 }
+
+func checkCommand(command string, args string) {
+  if cmd, ok := commands[command]; ok {
+    cmd(args) // execute the command
+  } else {
+    fmt.Printf("Command %s not found\n", command)
+  }
+}
+
+func exitCommand(args string) {
+  number, err := strconv.Atoi(args)
+  if err != nil {
+    fmt.Printf("%v - %s: not a valid number\n", number, err)
+    return
+  }
+  os.Exit(number)
+}
+func main() {
+  for {
+    fmt.Fprint(os.Stdout, "$ ")
+
+    // Wait for user input
+    input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+    // fmt.Printf("%s: command not found\n", strings.TrimSpace(input))
+    if err != nil {
+      fmt.Printf("%s: invalid input\n", input)
+    }
+
+    trimmpedInput := strings.TrimSpace(input)
+    handleCommands(trimmpedInput)
+  }
+}
+
+// comment again
+func handleCommands(input string) {
+  cmd, args, _ := strings.Cut(input, " ")
+  checkCommand(cmd, args)
+}
+
