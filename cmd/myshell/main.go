bfbfa74d5cf1e0918e15c13b9882ae9e558ac760
vietbui99 <buiquocviet99@gmail.com>
2025-01-01 16:15:23 -0500

    retest

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -61,6 +61,7 @@ func checkCommand(command string, args []string) {
       err = cmd.Run()
       if err != nil {
         fmt.Printf("%s: command not found (2)\n", command)
+        // test internal
         return
       }
     }
