9293f2621e9795598f5afa94a07f5827d680932f
vietbui99 <buiquocviet99@gmail.com>
2025-01-04 01:35:21 -0500

    check if correct

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -170,6 +170,7 @@ func exitCommand(args string, redirect string, _ bool) {
 
 
 func echoCommand(args string, redirectFile string, isStderr bool) {
+    var output io.Writer
     if redirectFile != "" {
         // Open or create the file for writing
         file, err := os.Create(redirectFile)
@@ -180,15 +181,18 @@ func echoCommand(args string, redirectFile string, isStderr bool) {
 
         if isStderr {
             // Write to stderr and to the file
-            os.Stderr = file
-        }
+            output = io.MultiWriter(os.Stderr, file)
+        } else {
 
         // Write to the file
-        fmt.Fprintln(file, args)
+            output = io.MultiWriter(os.Stdout, file)
+        }
+    } else {
+      output = os.Stdout
     }
 
     // Default behavior: Print to stdout
-    fmt.Println(args)
+    fmt.Fprintln(output, args)
 }
 
 
